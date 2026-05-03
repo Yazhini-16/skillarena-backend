c1d07fd10cf6276f25d3970b7c5a4c44213853d0
Yazhini-16 <yazhinipersonal16@gmail.com>
2026-05-03 15:35:08 +0530

    success backend

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use('/api/payments', require('./routes/payment'));
 
 
 
-app.use((req, res) => res.status(404).json({ success: false, message: 'Route not found' }));
+
 app.use(errorHandler);
 
 app.use('/', (req, res) => {
